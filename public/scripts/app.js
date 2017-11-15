9d4a1e68e77f9f9ac43487efbcc7ce8e8a6b901b
naveenkhasyap <naveen.maltesh@gmail.com>
2017-11-15 20:57:16 +0530

    updated

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,9 +5,70 @@ console.log('app.js is running');
 //jsx- javascript xml
 var appRoot = document.getElementById('app');
 var temp = React.createElement(
-  'h1',
-  null,
-  'Indecision App!'
+    'div',
+    null,
+    React.createElement(
+        'i',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Indecision App!'
+        ),
+        React.createElement(
+            'p',
+            null,
+            'this is a paragrah'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'one'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'two'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'three'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'four'
+            )
+        )
+    )
 );
 
-ReactDOM.render(temp, appRoot);
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'i',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            ' Naveen Kumar M'
+        ),
+        React.createElement(
+            'p',
+            null,
+            ' Age: 26 '
+        ),
+        React.createElement(
+            'p',
+            null,
+            ' Location: Bangalore '
+        )
+    )
+);
+
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -3,6 +3,29 @@ console.log('app.js is running');
 
 //jsx- javascript xml
 var appRoot = document.getElementById('app');
-var temp = <h1>Indecision App!</h1>;  
+var temp = (
+    <div>
+        <i><h1>Indecision App!</h1>
+        <p>this is a paragrah</p>
+        <ol>
+            <li>one</li>
+            <li>two</li>
+            <li>three</li>
+            <li>four</li>
+        </ol></i>
+    </div>
+);  
 
-ReactDOM.render( temp, appRoot);
+
+var templateTwo = (
+    <div>
+        <i>
+        <h1> Naveen Kumar M</h1>
+        <p> Age: 26 </p>
+        <p> Location: Bangalore </p>
+        </i>
+    </div>
+
+);
+
+ReactDOM.render( templateTwo, appRoot);
